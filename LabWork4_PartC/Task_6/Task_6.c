bdc845b880d3f1d267d6424138ae9b37ca5e0324
ksywxd <xenia5224@gmail.com>
2025-10-21 00:01:14 +0300

    для нечетных

--- LabWork4_PartC/Task_6/Task_6.c
+++ LabWork4_PartC/Task_6/Task_6.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdint.h>
+#include <errno.h>
+#include <limits.h>
 
 void checkInputChoice(int* choice) {
     char line[1000];
@@ -59,23 +62,42 @@ void checkInputInt(uint32_t* n) {
 
         line[strcspn(line, "\n")] = 0;
 
+        if (line[0] == '\0') {
+            printf("Empty input.\n");
+            continue;
+        }
+
         int valid = 1;
         for (int i = 0; line[i] != '\0'; i++) {
-            if (!isdigit(line[i])) {
+            if (!isdigit((unsigned char)line[i])) {
                 valid = 0;
                 break;
             }
         }
-
-        if (valid && sscanf(line, "%d", k) == 1) {
-            if (*n < 1) {
-                printf("Input error\n");
-            } else {
-                break;
-            }
-        } else {
+        if (!valid) {
             printf("Incorrect input.\n");
+            continue;
         }
+
+        errno = 0;
+        char *endptr;
+        unsigned long val = strtoul(line, &endptr, 10);
+
+        if (*endptr != '\0') {
+            printf("Incorrect input.\n");
+            continue;
+        }
+        if (errno == ERANGE || val > UINT32_MAX) {
+            printf("Number too large.\n");
+            continue;
+        }
+        if (val < 1) {
+            printf("Number must be >= 1.\n");
+            continue;
+        }
+
+        *n = (uint32_t)val;
+        break;
     }
 }
 
@@ -84,7 +106,38 @@ void checkInputInt(uint32_t* n) {
 void Task() {
     uint32_t n;
     printf("Enter the order of the magic square: ");
-    checkInputInt(*n);
+    checkInputInt(&n);
+    uint32_t** arr = (uint32_t**)malloc(n * sizeof(uint32_t*));
+    for (int k = 0; k < n; k++) {
+        arr[k] = (uint32_t*)calloc(n, sizeof(uint32_t));
+    }
+    uint32_t i = 0;
+    uint32_t j = (n - 1) / 2;
+    for (uint32_t num = 1; num <= n * n; num++) {
+        uint32_t prevI = i;
+        uint32_t prevJ = j;
+        arr[i][j] = num;
+
+        i = (i == 0) ? n - 1 : i - 1;
+        j = (j == n - 1) ? 0 : j + 1;
+        if (arr[i][j] != 0) {
+            i = (prevI + 1) % n;
+            j = prevJ;
+        }
+    }
+
+    for (uint32_t r = 0; r < n; r++) {
+        for (uint32_t c = 0; c < n; c++) {
+            printf("%4u ", arr[r][c]);
+        }
+        printf("\n");
+    }
+
+
+    for (uint32_t k = 0; k < n; k++) {
+        free(arr[k]);
+    }
+    free(arr);
 }
 
 void Menu() {
