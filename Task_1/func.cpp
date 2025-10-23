902ef584cc87a77195cc5c90bf8012b8e35279f4
ksywxd <xenia5224@gmail.com>
2025-10-23 20:55:00 +0300

    тесты

---
+++ Task_1/func.cpp
@@ -0,0 +1,26 @@
+#include <func.h>
+#include <iostream>
+#include <sstream>
+
+void checkInputChoice(int& choice) {
+    while (true) {
+        std::cout << "Start?\n1.YES\n2.EXIT\n3.Show menu\n";
+        std::string line;
+        if (!std::getline(std::cin, line)) {
+            std::cout << "Input error.\n";
+            continue;
+        }
+        std::stringstream ss(line);
+        if (ss >> choice && ss.eof()) {
+            if (choice >= 1 && choice <= 3) {
+                break; // одно число в диапазоне и ничего больше
+            }
+            else {
+                std::cout << "Incorrect input. Enter 1, 2, or 3.\n";
+            }
+        }
+        else {
+            std::cout << "Incorrect input. Enter a single number.\n";
+        }
+    }
+}
---
+++ Task_1/include/func.h
@@ -0,0 +1,6 @@
+#ifndef FUNC_H
+#define FUNC_H
+
+void checkInputChoice(int& choice);
+
+#endif //FUNC_H
